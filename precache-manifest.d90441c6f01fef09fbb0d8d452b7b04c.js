3f19ab2d1a0013e797ea462d5ba04e016bc8a2d5
Eros Casas <ecasas182015@gmail.com>
2020-10-18 14:12:10 -0500

    Updates

---
+++ precache-manifest.d90441c6f01fef09fbb0d8d452b7b04c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f91d7f86cca08cc2c09f9a505467cb1",
+    "url": "/CasasrobotFriends/index.html"
+  },
+  {
+    "revision": "98e88cae091b8680079f",
+    "url": "/CasasrobotFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c4a79a8bd3c8bdca42ab",
+    "url": "/CasasrobotFriends/static/css/main.9c24e0dc.chunk.css"
+  },
+  {
+    "revision": "98e88cae091b8680079f",
+    "url": "/CasasrobotFriends/static/js/2.cbb87561.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CasasrobotFriends/static/js/2.cbb87561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4a79a8bd3c8bdca42ab",
+    "url": "/CasasrobotFriends/static/js/main.e2386cd1.chunk.js"
+  },
+  {
+    "revision": "4e8a23c5efb698aea2f3",
+    "url": "/CasasrobotFriends/static/js/runtime-main.85cfe165.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/CasasrobotFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
